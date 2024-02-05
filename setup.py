ce463b4fbd8c9acd75e7d9942360128222087555
ritik <ritik.nimoy@gemseducation.com>
2024-02-05 13:46:48 +0530

    d

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="register",
+    version="v0.0.1",
+    install_requires=[
+        "Django", 
+        "redis",
+        "requests",
+        "djangorestframework",
+    ],
+    author="<your-name>",
+    author_email="<your-email>",
+    description="Django project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/<your-github-handle>/<your-repo>",
+    project_urls={
+        "Bug Tracker": "https://github.com/<your-github-handle>/<your-repo>/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="."),
+    python_requires=">=3.9",
+    scripts=["manage.py"],
+)
