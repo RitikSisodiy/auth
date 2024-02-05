1c318c1b641b45088af5dccff341745ea6273a12
ritik <ritik.nimoy@gemseducation.com>
2024-02-05 13:48:31 +0530

    d

--- setup.py
+++ setup.py
@@ -28,6 +28,5 @@ setuptools.setup(
     ],
     package_dir={"": "."},
     packages=setuptools.find_packages(where="."),
-    python_requires=">=3.9",
-    scripts=["manage.py"],
+    python_requires=">=3.9"
 )
